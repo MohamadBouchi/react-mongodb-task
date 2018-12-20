01c6b5c173db780027a68a2be642f234ec523d14
mohamedsubhi <mohamedsubhibouchi@gmail.com>
2018-12-20 09:27:37 +0100

    monoose installed task type created

--- client/src/components/Layout/SignedInLinks.js
+++ client/src/components/Layout/SignedInLinks.js
@@ -5,6 +5,7 @@ const SignedInLinks = () => {
     return (
         <ul className='right'>
             {/* <li><NavLink to='/createproject'>New Task</NavLink></li> */}
+            <li><NavLink to='/'>Create Task</NavLink></li>
             <li><NavLink to='/'>Log Out</NavLink></li>
             <li><NavLink to='/' className='btn btn-floating pink lighten-1'>Mo</NavLink></li>
         </ul>
--- server/api.js
+++ server/api.js
@@ -1,12 +1,60 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const graphQlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+const mongoose = require ('mongoose');
 const app = express();
-
+const tasks = [];
 app.use(bodyParser.json());
 
+const conn = mongoose.connect('mongodb://localhost:27017/ToDo', { useNewUrlParser: true }).then(() => {
+    app.listen(3001);
+    console.log("connected");
+}).catch( err => {
+    console.log(err);
+});
 
-app.get('/', (req, res, next) => {
-    res.send("Hi");
-})
-app.listen(3001);
+app.use('/graphql', graphQlHttp({
+    schema: buildSchema(`
+        type Task {
+            _id: ID!
+            title: String!
+            description: String!
+            date: String!
+        }
+
+        input TaskInput {
+            title: String!
+            description: String!
+            date: String!
+        }
+
+        type RootQuery {
+            tasks: [Task!]!
+        }
+
+        type RootMutation {
+            createTask(taskInput: TaskInput): Task
+        }
+        schema {
+            query: RootQuery 
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        tasks: () => {
+            return tasks;
+        },
+        createTask: (args) => {
+            const task = {
+                _id: Math.random().toString(),
+                title: args.taskInput.title,
+                description: args.taskInput.description,
+                date: args.taskInput.date
+            };
+            tasks.push(task);
+            return task;
+        }
+    },
+    graphiql: true
+}));
