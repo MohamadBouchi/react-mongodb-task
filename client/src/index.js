0e0256913c54b70fe3692281ef9079ceaf741c1b
Mohamed Bouchi <mohamedbouchi@Mohameds-iMac.local>
2018-12-26 13:24:41 +0100

    react-redux installed

--- client/src/index.js
+++ client/src/index.js
@@ -7,11 +7,15 @@ import 'materialize-css/dist/js/materialize.js';
 import 'materialize-css';
 import 'materialize-css/dist/css/materialize.min.css';
 import ApolloClient from "apollo-boost";
+import { createStore } from 'redux'
+import { Provider }from 'react-redux'
+import rootReducer from './reducers/rootReducer'
 
+const store = createStore(rootReducer);
 export const client = new ApolloClient({
   uri: "http://localhost:3001/graphql"
 });
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
